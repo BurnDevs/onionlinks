51e9ce71087f90b6dafc8876ab008c1785c9b20f
BurnDev's <55354316+BurnDevs@users.noreply.github.com>
2019-09-24 10:43:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,74 @@
-###################################
-#     Criado por Ryan Aragão      #
-#          18/02/2019             #
-# Para mais scripts siga no github#
-# https://github.com/RyanAragao2  #
-###################################
+####################################
+#      Criado por Ryan Aragão      #
+#          18/02/2019              #
+# Para mais scripts siga no github #
+#   https://github.com/BurnDevs/   #
+####################################
 from googlesearch import search
+from clint.textui import colored
+import platform
+import os
 
 # Apresentação
-print("Para entender melhor sobre a ferramenta")
-print("Ou para mais scripts, siga no github:")
-print("https://github.com/RyanAragao2")
-print("")
-input('Aperte ENTER para continuar')
+print("""
+             _             _ _       _        
+  ___  _ __ (_) ___  _ __ | (_)_ __ | | _____ 
+ / _ \| '_ \| |/ _ \| '_ \| | | '_ \| |/ / __|
+| (_) | | | | | (_) | | | | | | | | |   <\__ 
+ \___/|_| |_|_|\___/|_| |_|_|_|_| |_|_|\_\___/
+                                              
+      Criado por Ryan Aragão (BurnDev's)
+""")
 
 # Pegar tipo de conteúdo
-print("Exemplos de conteúdo: Hacking, Forum, eBooks")
-conteudo = str(input("coloque o conteúdo:"))
+print("Exemplos de conteúdo: Hacking, Forum, eBooks.")
+conteudo = str(input("coloque o conteúdo: "))
 
 # Dork para encontrar os URLS/LINKS/SITES
 dork = f'{conteudo} site:onion.link | site:onion.cab | site:onion.sh | site:tor2web.fi | site:onion.direct'
 
-# Menu de escolhas/seleção
+
+# definindo funçoes
 
 
 def menu():
-    banner = '''
-    +-------------------------------------------------------+
-    |          Script para buscar sites da Onion            |
-    | 1. Enquanto não estiver pronto o txt ficara em branco |
-    | 2. Tire o HTTP/HTTPS do URL para usa-lo no TOR.       |
-    | 3. Tire tudo que tiver depois do .onion               |
-    |     Exemplo Certo: exemplo293884.onion                |
-    |     Exemplo Errado: https://exemplo283874.onion.sh/   |
-    +-------------------------------------------------------+
-    '''
+    print('''
+    1. Será criado um TXT com os URLS, caso esteja em branco, aguarde o programa finalizar.
+    2. Antes de utilizar os links, retire HTTP:// HTTPS://
+    3. Retire tudo que tiver após o .onion do link.
+    ''')
 
-    print(banner)
+def opcoes():
     print("0 - Sair")
     print("1 - Procurar sites onion")
+    print("2 - Ver Instruções")
+
+
+def limpar():
+    sistema = platform.platform()
+    if "win" in sistema.lower():
+        cmd_limpar = "cls"
+    else:
+        cmd_limpar = "clear"
+
+    os.system(cmd_limpar)
 
 
 while True:
-    menu()
+    opcoes()
     menuzinho = int(input("Selecione uma opção: "))
     if menuzinho == 0:
-        print("Até a proxima")
+        limpar()
         break
     elif menuzinho == 1:
+        limpar()
         with open("sitesonion.txt", "w") as stream:
             for url in search(dork, stop=50 or 1):
                 print(url, file=stream)
-                print("Os URLS estão sendo salvos em um arquivo chamado: sitesonion.txt")
-                print("Aguarde o script encerrar")
+    elif menuzinho == 2:
+        limpar()
+        menu()
+        input("Pressione ENTER para continuar")
+        limpar()
     else:
         print("Essa opção não existe, tente novamente!")
